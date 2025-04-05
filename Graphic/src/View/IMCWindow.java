c5155900ca102431201d33ada4dd6b9aa5d2615c
Emanuel Tavecia <emanueltavecia@hotmail.com>
2025-04-04 21:18:54 -0300

    feat: add imc calculation method, show in screen the result and refactor code

--- Graphic/src/View/IMCWindow.java
+++ Graphic/src/View/IMCWindow.java
@@ -1,22 +1,26 @@
 package View;
 
+import java.awt.event.ActionEvent;
+
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.*;
 
 public class IMCWindow extends JFrame {
-    private int WINDOW_WIDTH = 350;
-    private int WINDOW_HEIGHT = 500;
-    private int HEADER_HEIGHT = 68;
-    private int HEADER_MARGIN_TOP = 8;
-    private int MARGIN_LEFT = 10;
-    private int INPUT_WIDTH = 280;
-    private int INPUT_HEIGHT = 30;
-    private int BUTTON_WIDTH = 300;
-    private int BUTTON_HEIGHT = 40;
-    private int LABEL_HEIGHT = 25;
-    private int BASE_SPACING = 15;
+    private static final int WINDOW_WIDTH = 350;
+    private static final int WINDOW_HEIGHT = 500;
+    private static final int HEADER_HEIGHT = 68;
+    private static final int HEADER_MARGIN_TOP = 8;
+    private static final int MARGIN_LEFT = 10;
+    private static final int INPUT_WIDTH = 280;
+    private static final int INPUT_HEIGHT = 30;
+    private static final int BUTTON_WIDTH = 300;
+    private static final int BUTTON_HEIGHT = 40;
+    private static final int LABEL_HEIGHT = 25;
+    private static final int BASE_SPACING = 15;
+    private static final String EMPTY_PLACEHOLDER = "___________";
 
+    private JButton calculateButton;
     private JTextField weightField, heightField;
     private JLabel resultIMC, resultIdealWeight, resultInterpretation;
 
@@ -29,7 +33,6 @@ public class IMCWindow extends JFrame {
 
         createHeaderComponents();
         createFormComponents();
-        createResultComponents();
         setVisible(true);
     }
 
@@ -54,77 +57,116 @@ public class IMCWindow extends JFrame {
     }
 
     public void createFormComponents() {
-        int currentY = HEADER_HEIGHT + BASE_SPACING;
+        int currentYFormSection = HEADER_HEIGHT + BASE_SPACING;
 
         JLabel labelWeight = new JLabel("Peso");
-        labelWeight.setBounds(MARGIN_LEFT, currentY, 100, LABEL_HEIGHT);
+        labelWeight.setBounds(MARGIN_LEFT, currentYFormSection, 100, LABEL_HEIGHT);
         getContentPane().add(labelWeight);
 
-        currentY += LABEL_HEIGHT;
+        currentYFormSection += LABEL_HEIGHT;
         weightField = new JTextField();
-        weightField.setBounds(MARGIN_LEFT, currentY, INPUT_WIDTH, INPUT_HEIGHT);
+        weightField.setBounds(MARGIN_LEFT, currentYFormSection, INPUT_WIDTH, INPUT_HEIGHT);
         getContentPane().add(weightField);
 
         JLabel labelWeightUnit = new JLabel("Kg");
-        labelWeightUnit.setBounds(MARGIN_LEFT + INPUT_WIDTH, currentY, 50, INPUT_HEIGHT);
+        labelWeightUnit.setBounds(MARGIN_LEFT + INPUT_WIDTH, currentYFormSection, 50, INPUT_HEIGHT);
         getContentPane().add(labelWeightUnit);
 
-        currentY += INPUT_HEIGHT + BASE_SPACING;
+        currentYFormSection += INPUT_HEIGHT + BASE_SPACING;
         JLabel labelHeight = new JLabel("Altura");
-        labelHeight.setBounds(MARGIN_LEFT, currentY, 100, LABEL_HEIGHT);
+        labelHeight.setBounds(MARGIN_LEFT, currentYFormSection, 100, LABEL_HEIGHT);
         getContentPane().add(labelHeight);
 
-        currentY += LABEL_HEIGHT;
+        currentYFormSection += LABEL_HEIGHT;
         heightField = new JTextField();
-        heightField.setBounds(MARGIN_LEFT, currentY, INPUT_WIDTH, INPUT_HEIGHT);
+        heightField.setBounds(MARGIN_LEFT, currentYFormSection, INPUT_WIDTH, INPUT_HEIGHT);
         getContentPane().add(heightField);
 
         JLabel labelHeightUnit = new JLabel("m");
-        labelHeightUnit.setBounds(MARGIN_LEFT + INPUT_WIDTH, currentY, 50, INPUT_HEIGHT);
+        labelHeightUnit.setBounds(MARGIN_LEFT + INPUT_WIDTH, currentYFormSection, 50, INPUT_HEIGHT);
         getContentPane().add(labelHeightUnit);
 
-        currentY += INPUT_HEIGHT + BASE_SPACING;
-        JButton calculateButton = new JButton("CALCULAR");
-        calculateButton.setBounds(MARGIN_LEFT, currentY, BUTTON_WIDTH, BUTTON_HEIGHT);
-        getContentPane().add(calculateButton);
-    }
+        currentYFormSection += INPUT_HEIGHT + BASE_SPACING;
 
-    public void createResultComponents() {
-        int currentY = HEADER_HEIGHT + 4 * (INPUT_HEIGHT + BASE_SPACING) + BUTTON_HEIGHT + 2 * BASE_SPACING;
+        calculateButton = new JButton(new AbstractAction("CALCULAR") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (weightField.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(weightField, "Peso é obrigatório");
+                    weightField.requestFocus();
+                    return;
+                }
+                if (heightField.getText().isEmpty()) {
+                    JOptionPane.showMessageDialog(heightField, "Altura é obrigatório");
+                    heightField.requestFocus();
+                    return;
+                }
+                try {
+                    double weight = Double.parseDouble(weightField.getText());
+                    double height = Double.parseDouble(heightField.getText());
+                    if (weight <= 0 || height <= 0) {
+                        JOptionPane.showMessageDialog(calculateButton, "Peso e altura devem ser positivos");
+                        return;
+                    }
+                    double imc = calculateIMC(weight, height);
+
+                    resultIMC.setText(String.format("%.2f", imc));
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(calculateButton, "Peso e altura devem ser números válidos");
+                }
+            }
+        });
+        calculateButton.setBounds(MARGIN_LEFT, currentYFormSection, BUTTON_WIDTH, BUTTON_HEIGHT);
+        getContentPane().add(calculateButton);
+
+        int currentYResultSection = HEADER_HEIGHT + 4 * (INPUT_HEIGHT + BASE_SPACING) + BUTTON_HEIGHT + 2 * BASE_SPACING;
 
         JLabel resultLabel = new JLabel("Resultado");
-        resultLabel.setBounds(MARGIN_LEFT, currentY, 100, LABEL_HEIGHT);
+        resultLabel.setBounds(MARGIN_LEFT, currentYResultSection, 100, LABEL_HEIGHT);
         getContentPane().add(resultLabel);
 
-        currentY += LABEL_HEIGHT;
+        currentYResultSection += LABEL_HEIGHT;
         JLabel labelIdealWeight = new JLabel("Peso ideal");
-        labelIdealWeight.setBounds(MARGIN_LEFT, currentY, 100, LABEL_HEIGHT);
+        labelIdealWeight.setBounds(MARGIN_LEFT, currentYResultSection, 100, LABEL_HEIGHT);
         getContentPane().add(labelIdealWeight);
 
-        resultIdealWeight = new JLabel("_");
-        resultIdealWeight.setBounds(120, currentY, 100, LABEL_HEIGHT);
+        resultIdealWeight = new JLabel(EMPTY_PLACEHOLDER);
+        resultIdealWeight.setBounds(120, currentYResultSection, 100, LABEL_HEIGHT);
         getContentPane().add(resultIdealWeight);
 
         JLabel labelIMC = new JLabel("IMC");
-        labelIMC.setBounds(200, currentY, 50, LABEL_HEIGHT);
+        labelIMC.setBounds(200, currentYResultSection, 50, LABEL_HEIGHT);
         getContentPane().add(labelIMC);
 
-        resultIMC = new JLabel("_");
-        resultIMC.setBounds(250, currentY, 100, LABEL_HEIGHT);
+        resultIMC = new JLabel(EMPTY_PLACEHOLDER);
+        resultIMC.setBounds(250, currentYResultSection, 100, LABEL_HEIGHT);
         getContentPane().add(resultIMC);
 
-        currentY += LABEL_HEIGHT;
+        currentYResultSection += LABEL_HEIGHT;
         JLabel labelInterpretation = new JLabel("Interpretação");
-        labelInterpretation.setBounds(MARGIN_LEFT, currentY, 100, LABEL_HEIGHT);
+        labelInterpretation.setBounds(MARGIN_LEFT, currentYResultSection, 100, LABEL_HEIGHT);
         getContentPane().add(labelInterpretation);
 
-        resultInterpretation = new JLabel("_");
-        resultInterpretation.setBounds(120, currentY, 200, LABEL_HEIGHT);
+        resultInterpretation = new JLabel(EMPTY_PLACEHOLDER);
+        resultInterpretation.setBounds(120, currentYResultSection, 200, LABEL_HEIGHT);
         getContentPane().add(resultInterpretation);
 
-        currentY += LABEL_HEIGHT + BASE_SPACING;
-        JButton clearButton = new JButton("LIMPAR");
-        clearButton.setBounds(MARGIN_LEFT, currentY, BUTTON_WIDTH, BUTTON_HEIGHT);
+        currentYResultSection += LABEL_HEIGHT + BASE_SPACING;
+        JButton clearButton = new JButton(new AbstractAction("LIMPAR") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                weightField.setText("");
+                heightField.setText("");
+                resultIMC.setText(EMPTY_PLACEHOLDER);
+                resultIdealWeight.setText(EMPTY_PLACEHOLDER);
+                resultInterpretation.setText(EMPTY_PLACEHOLDER);
+            }
+        });
+        clearButton.setBounds(MARGIN_LEFT, currentYResultSection, BUTTON_WIDTH, BUTTON_HEIGHT);
         getContentPane().add(clearButton);
+    }
+
+    private double calculateIMC(double weight, double height) {
+        return (weight / (height * height));
     }
 }
